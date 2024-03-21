7a4fa80fd7e6e0887e1bff6fbc7dc0a3d5d011de
goyadev <goyazheng@gmail.com>
2024-03-21 21:55:03 +0800

    created components for footer, header, hero and nav

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import NavBar from "./NavBar";
+
+export function Header() {
+  return (
+    <>
+      <h1>
+        I am the header. I contain the logo, navigation, search bar, login and
+        register
+      </h1>
+      {/* logo*/}
+      {/* nav */}
+      <NavBar />
+      {/* search bar*/}
+      {/* login button*/}
+      {/* register button*/}
+    </>
+  );
+}
+export default Header;
