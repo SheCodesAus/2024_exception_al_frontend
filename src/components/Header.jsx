0dc23e0a3e8fead57b3731d98931df82b604e08a
goyadev <goyazheng@gmail.com>
2024-03-22 20:23:28 +0800

    Added styling trial for tailwind to skeleton

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import NavBar from "./NavBar";
+import "../index.css";
 
 export function Header() {
   return (
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
+import "../index.css";
+
 export default function Layout() {
   return (
     <>
