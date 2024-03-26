67244f4c2980a8f1d48011b7268e0b7ed6045df7
goyadev <goyazheng@gmail.com>
2024-03-26 14:54:22 +0800

    created page and psuedo coded out shape of page. Need to consider hooks for the next step once backend deployed

---
+++ src/pages/IdeaDetail.jsx
@@ -0,0 +1,29 @@
+// idea image
+
+// contains idea name
+
+// also contains idea short description
+
+// end image tag
+
+// section for left panel
+
+// section start for date cost
+
+// contains date/potential date/tbc
+
+// contains cost
+
+// end section
+
+// overview of project
+
+// end left panel section
+
+// right side panel section start
+
+// contains whether workshop numbers are open or closed
+
+// contains whether mentors are being looked for or not
+
+// information about the organiser of the Workshop Idea
