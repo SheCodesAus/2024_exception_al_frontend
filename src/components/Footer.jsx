a260d9b4704823296e1bfb19f4ef68aeae3583e3
goyadev <goyazheng@gmail.com>
2024-03-26 12:10:00 +0800

    refreshed logo in header, footer and favicon with planidea logo as an SVG with a clear background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ function Footer() {
   return (
     <footer className=" bg-primary text-sm">
       <div className="mx-8 my-8">
-        <img src="logo.svg" alt="planet expressions" />
+        <img className="size-24" src="planidea-light.svg" alt="planidea logo" />
         <section className="flex flex-col sm:flex-row md:flex-row lg:flex-row">
-          <h3>Copyright {new Date().getFullYear()} | Planet Expressions</h3>
+          <h3>Copyright {new Date().getFullYear()} | planidea</h3>
           <a href="/" className="hover:text-secondary">
             Privacy Policy
           </a>
