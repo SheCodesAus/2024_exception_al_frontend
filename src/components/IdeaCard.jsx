770f536389a9b6b3e64cf94be28dc7d2dd689947
goyadev <goyazheng@gmail.com>
2024-03-28 20:49:04 +0800

    added some flex to the styling of idea card with some mock data for easier integration when the backend is deployed. Added APIs which will need to be looked at once back end deployed. Based them off presvious project with an update to some areas. Only change to text input was ignoring an error in vs code. Didn't change any actual code

--- src/components/IdeaCard.jsx
+++ src/components/IdeaCard.jsx
@@ -1,26 +1,56 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 
-function IdeaCard() {
-  //need a const link for idea//
+function IdeaCard({
+  id,
+  plannedDate,
+  image,
+  title,
+  description,
+  attendeeTarget,
+  mentorTarget,
+}) {
+  // const { workshopData } = props;
+  var date = new Date(plannedDate);
+  var formattedDate = date.toLocaleDateString();
 
   return (
-    <div className="container mx-auto border border-black rounded-lg"> 
-        <img alt="Women holding pottery" class="rounded-lg w-auto pb-2" src="../women-holding-pottery.jpg"></img>
-        <h1 className="text-xl p-2 md:p-4">IdeaWorkshop Title</h1>
-                <p className="ml-4">attendees</p>
-                <p className="ml-4">description</p>
-                <p className="ml-4">date</p>
-                {/*<p>{ideaData.date}</p>
-                <p>{ideaData.description}</p>
-                <p>{ideaData.attendees}</p>
-                <p>{ideaData.mentors}</p>
-                <button onClick={() => alert(`View details of ${ideaData.title}`)}>
-                    More Info
-                </button> 
-                */}
-            
+    <div className="flex flex-col items-center justify-center container mx-auto border border-dark rounded-lg max-w-96">
+      <Link to={`/idea/${id}`}>
+        <img
+          src={image}
+          alt={`${title} picture of workshop`}
+          className="object-fill rounded-lg pb-2"
+        ></img>
+        <h2 className="text-xl p-2 md:p-4">{title}</h2>
+      </Link>
+      <section className="items-start">
+        <p className="ml-4">Planned Date: {formattedDate}</p>
+        <p className="ml-4">Description: {description}</p>
+        <p className="ml-4">Attendee Target:{attendeeTarget}</p>
+        <p className="ml-4">Mentor Target: {mentorTarget}</p>
+      </section>
+      <Link to={`/idea/${id}`}>More Info</Link>
     </div>
   );
 }
 
 export default IdeaCard;
+
+// <Link to={`/idea/${workshopData.id}`}>
+//         <img
+//           // src={workshopData.image}
+//           alt="Women holding pottery"
+//           className="rounded-lg w-auto pb-2"
+//           src="../women-holding-pottery.jpg"
+//         ></img>
+//         <h2 className="text-xl p-2 md:p-4">{workshopData.title}</h2>
+//       </Link>
+//       <p className="ml-4">Planned Date: {formattedDate}</p>
+//       <p className="ml-4">Description: {workshopData.description}</p>
+//       <p className="ml-4">{workshopData.attendee_target}</p>
+//       <p>{workshopData.mentor_target}</p>
+//       <button onClick={() => alert(`View details of ${ideaData.title}`)}>
+//         More Info
+//       </button>
+//     </div>
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,8 +1,16 @@
+/* eslint-disable react/prop-types */
 export default function TextInput({ size, label, ...rest }) {
   return (
-    <div className={`flex flex-col mb-4 ${size === "sm" ? "sm:w-1/2-gap-4" : ""}`}>
-      <label htmlFor={rest.name} className="text-sm font-medium pb-1">{label}</label>
-      <input {...rest} className="border-2 border-solid border-greyscale-400 rounded-md bg-greyscale-200 px-3 py-2" />
+    <div
+      className={`flex flex-col mb-4 ${size === "sm" ? "sm:w-1/2-gap-4" : ""}`}
+    >
+      <label htmlFor={rest.name} className="text-sm font-medium pb-1">
+        {label}
+      </label>
+      <input
+        {...rest}
+        className="border-2 border-solid border-greyscale-400 rounded-md bg-greyscale-200 px-3 py-2"
+      />
     </div>
   );
 }
