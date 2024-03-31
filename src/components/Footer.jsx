06e0cf33e241e437d33f993b9bd8dd580887bd2f
goyadev <goyazheng@gmail.com>
2024-03-31 20:02:42 +0800

    getting closer, but not quite there yet. Would love some advice on why when I try flex on the socials, the instagram icon goes away

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,43 @@
 function Footer() {
   return (
-    <footer className="w-full bg-primary text-sm p-4">
-      <div className="container mx-auto">
+    <footer className="w-full bg-primary text-sm p-4 ">
+      <div className="container mx-auto  ">
         <img className="size-24" src="planidea-light.svg" alt="planidea logo" />
-        <section className="flex flex-col sm:flex-row md:flex-row lg:flex-row">
-          <div>
+        <section className="flex flex-col sm:flex-row  lg:flex-row space-x-8">
+          <section>
             <h3>Copyright {new Date().getFullYear()} | planidea</h3>
-          </div>
-          <div className="max-w-12">
+          </section>
+          <section className="">
             <a href="https://www.instagram.com/shecodesaus/?hl=en">
-              <img src="/Instagram_Glyph_Black.svg" alt="Instagram Logo" />
+              <img
+                src="/Instagram_Glyph_Black.svg"
+                alt="Instagram Logo"
+                className="max-w-12"
+              />
             </a>
 
             <a href="https://www.facebook.com/yourpage">
-              <img src="/Facebook_Glyph.svg" alt="Facebook Logo" />
+              <img
+                src="/Facebook_Glyph.svg"
+                alt="Facebook Logo"
+                className="max-w-12"
+              />
             </a>
-          </div>
-          <a href="/" className="hover:text-secondary">
-            Privacy Policy
-          </a>
-          <a href="/" className="hover:text-secondary">
-            Terms & Conditions
-          </a>
-          <a href="/" className="hover:text-secondary">
-            Cookie Policy
-          </a>
-          <a href="/" className="hover:text-secondary">
-            Contact
-          </a>
+          </section>
+          <section className="flex space-x-4 flex-row">
+            <a href="/" className="hover:text-secondary">
+              Privacy Policy
+            </a>
+            <a href="/" className="hover:text-secondary">
+              Terms & Conditions
+            </a>
+            <a href="/" className="hover:text-secondary">
+              Cookie Policy
+            </a>
+            <a href="/" className="hover:text-secondary">
+              Contact
+            </a>
+          </section>
         </section>
       </div>
     </footer>
