6f1a17fe6bd4626c308ea9a0dd73acd8f2bc021d
goyadev <goyazheng@gmail.com>
2024-03-31 19:28:04 +0800

    social icons added with max width added. Need to change layout for tablet and desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,18 @@ function Footer() {
       <div className="container mx-auto">
         <img className="size-24" src="planidea-light.svg" alt="planidea logo" />
         <section className="flex flex-col sm:flex-row md:flex-row lg:flex-row">
-          <h3>Copyright {new Date().getFullYear()} | planidea</h3>
+          <div>
+            <h3>Copyright {new Date().getFullYear()} | planidea</h3>
+          </div>
+          <div className="max-w-12">
+            <a href="https://www.instagram.com/shecodesaus/?hl=en">
+              <img src="/Instagram_Glyph_Black.svg" alt="Instagram Logo" />
+            </a>
+
+            <a href="https://www.facebook.com/yourpage">
+              <img src="/Facebook_Glyph.svg" alt="Facebook Logo" />
+            </a>
+          </div>
           <a href="/" className="hover:text-secondary">
             Privacy Policy
           </a>
