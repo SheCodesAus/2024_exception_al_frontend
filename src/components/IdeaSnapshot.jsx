8be6b25c77b417488a9d41e6300051d92062d1e1
Suan Choi <siasiarhdwn@gmail.com>
2024-04-05 23:37:42 +0800

    Feat: eoi form
    - Add eoi form
    - Add modal component
    - Add logic to fetch post EOI based on workshop owner and eoi type
    - Restyle ideacard

--- src/components/IdeaSnapshot.jsx
+++ src/components/IdeaSnapshot.jsx
@@ -1,30 +1,74 @@
 import IdeaCard from "./IdeaCard";
 import Button from "./Button";
 import useWorkshops from "../hooks/use-workshops";
+import LoadingSpinner from "./LoadingSpinner";
+import useModal from "../hooks/use-modal";
+import Modal from "./Modal";
+import EOIForm from "./EOIForm";
+import { useState } from "react";
+import Toast from "./Toast";
+import useToast from "../hooks/use-toast";
 
-function IdeaSnapshot({itemNumber}) {
-  const { workshops } = useWorkshops();
+function IdeaSnapshot({ itemNumber, orderBy }) {
+  const { workshops, isLoading } = useWorkshops();
+  const { isOpen, toggleModal } = useModal();
+  const [selectedWorkshopId, setSelectedWorkshopId] = useState(null);
+  const { showToast, isVisible } = useToast();
+  const [type, setType] = useState("");
+  const handleEOIClick = (workshopId, type) => {
+    setSelectedWorkshopId(workshopId);
+    setType(type);
+    showToast();
+  };
+
+  const sortedWorkshops = [...workshops].sort((a, b) => {
+    const dateA = new Date(a.created_at);
+    const dateB = new Date(b.created_at);
+    return orderBy === "latest" ? dateB - dateA : dateA - dateB;
+  });
+
   return (
-    <div className="flex flex-col justify-around items-center container mx-auto p-4">
-      <h2 className="text-xl font-bold my-6">Latest Ideas</h2>
-      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {workshops.map((workshop, index) => {
-          if (index < itemNumber) {
-            return <IdeaCard workshop={workshop} key={index} />;
-          }
-        })}
+    <>
+      <div className="flex flex-col justify-around items-center mx-auto p-4">
+        {isLoading ? (
+          <div className="h-full w-full flex items-center justify-center">
+            <LoadingSpinner />
+          </div>
+        ) : (
+          <>
+            <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+              {sortedWorkshops.map((workshop, index) => {
+                if (index < itemNumber) {
+                  return (
+                    <IdeaCard
+                      workshop={workshop}
+                      key={index}
+                      toggleModal={toggleModal}
+                      handleClick={handleEOIClick}
+                    />
+                  );
+                }
+              })}
+            </div>
+          </>
+        )}
       </div>
-      <section className="m-5">
-        <Button
-          variant="link"
-          href={`/workshops/1`}
-          size="sm"
-          buttonStyle="solid"
-        >
-          View All Workshop Ideas
-        </Button>
-      </section>
-    </div>
+      {type === "error" ? (
+        <Toast
+          message="Sorry! Idea owner can't vote."
+          type="error"
+          isVisible={isVisible}
+        />
+      ) : (
+        <Modal open={isOpen} onClose={toggleModal}>
+          <EOIForm
+            workshopId={selectedWorkshopId}
+            onClose={toggleModal}
+            actionType={type}
+          />
+        </Modal>
+      )}
+    </>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button";
 import Hero from "../components/Hero";
 import IdeaSnapshot from "../components/IdeaSnapshot";
 
@@ -5,7 +6,18 @@ function Home() {
   return (
     <>
       <Hero />
-      <IdeaSnapshot itemNumber={6}/>
+      <h2 className="text-3xl text-center mt-16 mb-6">Featured Ideas</h2>
+      <IdeaSnapshot itemNumber={6} orderBy="latest" />
+      <div className="mx-auto my-6 w-fit">
+        <Button
+          variant="link"
+          href={`/workshops/1`}
+          size="sm"
+          buttonStyle="primary-outline"
+        >
+          View All Workshop Ideas
+        </Button>
+      </div>
     </>
   );
 }
