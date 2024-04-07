e7c02c87a8f498e7ec288e8f12ed314edd8b9a33
goyadev <goyazheng@gmail.com>
2024-04-07 16:37:48 +0800

    tried some different styling to make it look better

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,45 +4,42 @@ import FacebookLogo from "../assets/icons/facebook-2.svg";
 function Footer() {
   return (
     <footer className="w-full bg-primary text-sm p-4 ">
-      <div className="container mx-auto  ">
-        <img
-          className="size-24"
-          src="/planidea-light.svg"
-          alt="planidea logo"
-        />
-        <section className="flex flex-col sm:flex-row  lg:flex-row space-x-28">
-          <section>
-            <h3>Copyright {new Date().getFullYear()} | planidea</h3>
-          </section>
-          <section className="flex">
-            <a href="https://www.instagram.com/shecodesaus/?hl=en">
-              <img src={InstagramLogo} alt="Instagram Logo" className="w-8" />
+      <div className="container mx-auto flex flex-col sm:flex-col lg:flex-row justify-between items-center">
+        <div className="flex items-center space-x-4">
+          <img
+            className="size-24"
+            src="/planidea-light.svg"
+            alt="planidea logo"
+          />
+          <h3>Copyright {new Date().getFullYear()} | planidea</h3>
+        </div>
+        <div className="flex justify-center space-x-4 ">
+          <a href="https://www.instagram.com/shecodesaus/?hl=en">
+            <img src={InstagramLogo} alt="Instagram Logo" className="w-8" />
+          </a>
+          <a href="https://www.facebook.com/shecodesaustralia/">
+            <img src={FacebookLogo} alt="Facebook Logo" className="w-8" />
+          </a>
+        </div>
+        <div className="flex flex-col lg:flex-row">
+          <section className="flex space-x-4">
+            <a href="/privacy" className="hover:text-white">
+              Privacy Policy
             </a>
-
-            <a href="https://www.facebook.com/shecodesaustralia/">
-              <img src={FacebookLogo} alt="Facebook Logo" className="w-8" />
+            <a href="/termsandconditions" className="hover:text-white">
+              Terms & Conditions
+            </a>
+            <a
+              href="https://youtu.be/Ye8mB6VsUHw?si=t8GPJxh3FpjzYYim&t=10"
+              className="hover:text-white"
+            >
+              Cookie Policy
+            </a>
+            <a href="/contactus" className="hover:text-white">
+              Contact Us
             </a>
           </section>
-          <div>
-            <section className="flex space-x-4 flex-row">
-              <a href="/privacy" className="hover:text-white">
-                Privacy Policy
-              </a>
-              <a href="/termsandconditions" className="hover:text-white">
-                Terms & Conditions
-              </a>
-              <a
-                href="https://youtu.be/Ye8mB6VsUHw?si=t8GPJxh3FpjzYYim&t=10"
-                className="hover:text-white"
-              >
-                Cookie Policy
-              </a>
-              <a href="/contactus" className="hover:text-white">
-                Contact Us
-              </a>
-            </section>
-          </div>
-        </section>
+        </div>
       </div>
     </footer>
   );
